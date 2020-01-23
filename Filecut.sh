b4c3698ed4a728126996ee13ba7af194cd436610
AlbertoGila <albertogilanavarro@gmail.com>
2020-01-23 15:09:41 +0100

    Script de corte  mejorado

--- Filecut.sh
+++ Filecut.sh
@@ -6,10 +6,12 @@ num=$2
 output_dir=$3
 total_lines=$(wc -l ${file} | cut -d ' ' -f 1) #Extrae total de lineas
 cutfile_name=$(echo ${file} | grep -o '[^/]*$') #Crea nombre de salida, el ultimo campo de la ruta 
-for index in $(seq 1 ${total_lines}); do #Extrae lineas multiplo de num
-	if [ $((index % num)) -eq 0 ]; then
-		head -${index} ${file} | tail -1 >> ${cutfile_name}
+counter=1
+cat ${file} | while read line; do
+	if [ $((counter % num)) -eq 0 ]; then
+		echo ${line} >> ${cutfile_name}
 	fi
-done
-mv ${cutfile_name} ${output_dir} #Lleva los resultados al directorio
+	counter=$((counter+1))
+done 
+mv  ${cutfile_name} ${output_dir} #Lleva los resultados al directorio
 
